9245059f0e71acef2f7c5f33f90584941d699da7
Luca Del Zoppo <ldelzoppo1@gmail.com>
2026-05-17 08:14:01 +0200

    feat: initial xcode project setup

---
+++ Tempo/Tempo/TempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoApp.swift
+//  Tempo
+//
+//  Created by Luca Del Zoppo on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
